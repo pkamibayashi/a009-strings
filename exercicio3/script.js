4e8415ad0ce8ef974174fc74f3c63b8305e689d8
pkamibayashi <paulo_Kamibayashi@hotmail.com>
2022-08-31 14:19:41 -0300

    add Exercicio3 letra C

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -2,12 +2,12 @@
 
 const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
 const trocandoFrase = frase.replaceAll('verde', 'rosa').replaceAll('azul', 'laranja');
+const TrocandoFrase2= frase.replace('mas não deixe o gato sair', 'MAS NÃO DEIXE O GATO SAIR')
 
 console.log(`frase sem trocar as cores: ${frase}`)
 console.log(`frase trocando as cores: ${trocandoFrase}`)
-
-const TrocandoFrase2= frase.replace('mas não deixe o gato sair', 'MAS NÃO DEIXE O GATO SAIR')
 console.log(`Frase extra : ${TrocandoFrase2}`)
+
 
 
  
@@ -25,5 +25,7 @@ console.log(`Frase extra : ${TrocandoFrase2}`)
 // b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
 
 // c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+console.log(trocandoFrase.includes('verde'))
+console.log(trocandoFrase.includes('laranja'))
 
 // **EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
